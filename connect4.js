4439bbb277d1ac0bc9cc92fb709afd55dc5cfa0c
Ana Martínez Bohórquez <amartinezbo@unal.edu.co>
2023-05-28 12:39:07 -0600

    Update connect4.js

--- connect4.js
+++ connect4.js
@@ -8,31 +8,25 @@ let colorDisplay; // quadrille color cell
 let synth;
 let melodyPlaying = false;
 let numero;
-
-
 function preload() {
-
   ConfigJSON = loadJSON('configo.json');
 }
-
 function setup() {
   numero = parseInt(window.prompt("Please enter the number of pieces to make the connect N:", 4));
   boton = createCheckbox('musica', false);
   boton.position(10, 10);
   boton.changed(toggleMelody);
-
   synth = new Tone.MembraneSynth({
     oscillator: {
       type: 'sawtooth'
     },
     volume: -30
   }).toDestination();
-
   // Configuraciones de las piezas "Objeto literal"
   ficha = {
     position: { x: 0, y: 0 },
+    pieza: color('red'),
     colores: { j1: 'red', j2: 'yellow', Fondo: 'blue', Borde: 'white' },
-     pieza: color(this.colores.j1),
     n: numero,
     import: function(jsonPiece) {
       this.pieza = color(jsonPiece.color1);
@@ -42,16 +36,11 @@ function setup() {
       this.colores.Borde = jsonPiece.color4;
       //this.n=jsonPiece.n
     }
-    
   };
-  
-
   if (ConfigJSON != null)
     ficha.import(ConfigJSON);
-
   frameRate(60);
   createCanvas(ficha.n * 2 * Quadrille.CELL_LENGTH, 2 * ficha.n * Quadrille.CELL_LENGTH);
-
   // quadrille object initialization
   circulo = ({ cell: cell, cellLength: cellLength }) => {
     noStroke();
@@ -59,16 +48,13 @@ function setup() {
     ellipseMode(CORNER);
     ellipse(0, 0, cellLength, cellLength);
   };
-
   caer = createQuadrille(ficha.n + 3, 1);
   tablero = createQuadrille(ficha.n + 3, ficha.n + 2);
 }
-
   function draw() {
    
     background(ficha.colores.Fondo);
     // to display the quadrille a call to drawQuadrille is needed
-
     let params = {x:50,
                   y:150,
                   outlineWeight:5,
@@ -76,7 +62,6 @@ function setup() {
                   outline:ficha.colores.Borde,
                   colorDisplay: circulo,
                  }
-
     drawQuadrille(caer,{x:50,y:10,outlineWeight:5,outline:ficha.colores.Borde,
                        colorDisplay: circulo });
     drawQuadrille(tablero,params);
@@ -84,7 +69,6 @@ function setup() {
     caer.fill(0,ficha.position.x,ficha.pieza)
     controlador()
   }
-
   function keyPressed(){
     if(key === 'd'  || key == "ArrowRight"){
       if(ficha.position.x + 1<ficha.n+3){
@@ -92,7 +76,6 @@ function setup() {
         ficha.position.x += 1
         }
     }
-
     if(key === 'a'  || key == "ArrowLeft"){
       if(ficha.position.x -1 >-1){
         caer.clear(0)
@@ -106,7 +89,6 @@ function setup() {
         siguiente(turno)
         turno+=1
         setTimeout(ganador,50)
-
     }
   }
 }
@@ -139,19 +121,13 @@ function setup() {
       synth.triggerAttackRelease("G4", "8n");
     }
   }
-
-
-
 function mArray(a1,a2){
 for (let i = 0; i < a1.length; i++) {
     if(a1[i]!=a2[i])
         return false
 }
     return true
-
 }
-
-
 function ganador() {
   fila = ficha.position.y;
   colum = ficha.position.x;
@@ -163,44 +139,35 @@ function ganador() {
   for (i = 1; i < ficha.n; i++) {
     if (tablero.read(fila + i, colum) != null)
       if (mArray(tablero.read(fila + i, colum).levels, tipo)) l1 += 1;
-
     if (tablero.read(fila - i, colum) != null)
       if (mArray(tablero.read(fila - i, colum).levels, tipo)) l1 += 1;
-
     if (tablero.read(fila, colum + i) != null)
       if (mArray(tablero.read(fila, colum + i).levels, tipo)) l2 += 1;
-
     if (tablero.read(fila, colum - i) != null)
       if (mArray(tablero.read(fila, colum - i).levels, tipo)) l2 += 1;
-
     if (tablero.read(fila + i, colum + i) != null)
       if (mArray(tablero.read(fila + i, colum + i).levels, tipo)) l3 += 1;
-
     if (tablero.read(fila - i, colum - i) != null)
       if (mArray(tablero.read(fila - i, colum - i).levels, tipo)) l3 += 1;
-
     if (tablero.read(fila + i, colum - i) != null)
       if (mArray(tablero.read(fila + i, colum - i).levels, tipo)) l4 += 1;
-
     if (tablero.read(fila - i, colum + i) != null)
       if (mArray(tablero.read(fila - i, colum + i).levels, tipo)) l4 += 1;
 
     if (l1 > ficha.n-2 || l2 > ficha.n-2 || l3 > ficha.n-2 || l4 > ficha.n-2) {
       // Player wins
-         turno%2==0 ? window.alert("Ganó el Jugador 1"):window.alert("Ganó el Jugador 2")
+         turno%2==0 ? window.alert("Ganó el Jugador 1"):window.alert("Ganó el Jugador 2");
       tablero = createQuadrille(ficha.n+3,ficha.n+2);
       break;
     }
   }
 }
-
 function toggleMelody() {
   if (boton.checked()) {
     melodyPlaying = true;
     Tone.Transport.scheduleRepeat((time) => {
       playMelody(time);
     }, '8n');
-
     Tone.Transport.start();
   } else {
     melodyPlaying = false;
@@ -208,7 +175,6 @@ function toggleMelody() {
     index = 0;
   }
 }
-
 const notes = ['A3', 'D4', 'E4', 'F5', 'D5', 'E4', 'C4', 'D4', 'A3', 'A3'];
 let index = 0;
 
