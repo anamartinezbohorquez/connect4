111fe63983177d0dc64b745bdd919617228b6c1f
Fran7373 <111193312+Fran7373@users.noreply.github.com>
2023-05-29 10:40:02 -0500

    Update connect4.js

--- connect4.js
+++ connect4.js
@@ -191,7 +191,7 @@ console.log(notes)
 
 function playMelody(time) {
   if (melodyPlaying) {
-    let note = ficha.notas[index % ficha.notas.length];
+    let note = notes[index % notes.length];
     synth.triggerAttackRelease(note, ficha.ritmo, time);
     index++;
   }
